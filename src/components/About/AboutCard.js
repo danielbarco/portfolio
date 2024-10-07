ab9820eefac169df4003befd31575d46ca9db900
danielbarco <danielaabarco@gmail.com>
2024-10-07 11:27:32 +0200

    rewrite about me section

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,11 +9,11 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Daniel Barco </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            from <span className="purple"> Winterthur, Switzerland.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently a PhD candidate in Data Science at the University of Zurich. My research at the Centre for Artificial Intelligence (ZHAW) focuses on the development of deep learning algorithms for vision tasks.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
+            I have completed MSc in Applied Information and Data Science from the University of Applied Sciences of Luzern (HSLU), a MSSc in International and European Relations from the University of Linköping in Sweden and a BSc in Computer Science from the University of Petronas Technology in Malaysia.
             Mesra.
             <br />
             <br />
@@ -21,13 +21,13 @@ function AboutCard() {
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Cycling
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Cooking
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Hiking
             </li>
           </ul>
 
