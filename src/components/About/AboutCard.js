19a8e4af9b9753afeee0dc76d3ef316a7ca2162a
danielbarco <danielaabarco@gmail.com>
2024-10-07 11:28:29 +0200

    changed quote

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -32,7 +32,7 @@ function AboutCard() {
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Use data science for social good!"{" "}
           </p>
           <footer className="blockquote-footer">Daniel</footer>
         </blockquote>
