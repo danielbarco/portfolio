1103b5443d23c4ccaf99597bd90c816681bee6e6
danielbarco <danielaabarco@gmail.com>
2024-10-07 21:00:45 +0200

    improve github references

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -83,16 +83,6 @@ function NavBar() {
             </Nav.Item>
 
 
-            <Nav.Item className="fork-btn">
-              <Button
-                href="https://github.com/danielbarco/Portfolio"
-                target="_blank"
-                className="fork-btn-inner"
-              >
-                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
-                <AiFillStar style={{ fontSize: "1.1em" }} />
-              </Button>
-            </Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Container>
