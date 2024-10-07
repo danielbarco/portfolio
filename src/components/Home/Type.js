5cdc8505c8a6103ef7f57b6c519fdf287044bb35
danielbarco <danielaabarco@gmail.com>
2024-10-07 13:23:15 +0200

    Update typewriter-effect

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,10 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "PhD Candidate",
+          "AI Researcher",
+          "Data Scientist",
+          "Deep Learning Engineer",
         ],
         autoStart: true,
         loop: true,
