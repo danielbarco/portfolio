5d17fec53023f7056e27f5bb75bfe6efea9ffa5d
danielbarco <danielaabarco@gmail.com>
2024-10-08 16:46:40 +0200

    add review paper

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,7 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import scorpio from "../../Assets/Projects/scorpio.png";
 import malaria_detection from "../../Assets/Projects/malaria-detection.gif";
+import review from "../../Assets/Projects/review.png";
 
 function Projects() {
   return (
@@ -17,26 +18,37 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={scorpio}
+              imgPath={review}
               isBlog={false}
-              title="Scorpio website"
-              description="Website design for a local travel agency."
-              demoLink="https://scorpio.ch/"
+              title="Publication"
+              description="Artifact Reduction in 3D and 4D Cone-Beam Computed Tomography Images With Deep Learning: A Review"
+              pubLink="https://ieeexplore.ieee.org/abstract/document/10398205"
             />
           </Col>
+
 
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={malaria_detection}
               isBlog={false}
-              title="Malaria Detection"
+              title="Malaria Detection Webapp"
               description="Deep learning algorithm to detect malaria on blood smears."
               ghLink="https://github.com/danielbarco/malariadetection_webapp/tree/main"
             />
           </Col>
 
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={scorpio}
+              isBlog={false}
+              title="Scorpio Website"
+              description="Website design for a local travel agency."
+              demoLink="https://scorpio.ch/"
+            />
+          </Col>
 
         </Row>
       </Container>
