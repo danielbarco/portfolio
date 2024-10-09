3913fef43b0ab6d1ed4924e1b0d4ca692e4739fb
danielbarco <danielaabarco@gmail.com>
2024-10-09 08:16:46 +0200

    rmv unused imports

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -2,9 +2,7 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
-  SiPostman,
-  SiSlack,
-  SiVercel,
+
   SiMacos,
 } from "react-icons/si";
 
